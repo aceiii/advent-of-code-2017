ef18eca10777de22552a566535964b1063709e60
Borin Ouch <borin.ouch@gmail.com>
2017-12-07 22:52:04 -0800

    Add day 8 solutions

---
+++ day8b_highest_register.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import sys
+import re
+
+from operator import itemgetter, lt, gt, le, ge, eq, ne, add, sub
+from collections import defaultdict
+
+
+class Instruction(object):
+    ops = {
+        "<": lt,
+        ">": gt,
+        "<=": le,
+        ">=": ge,
+        "==": eq,
+        "!=": ne,
+        "inc": add,
+        "dec": sub,
+    }
+
+    def __init__(self, instr, cond):
+        self.instr = map(lambda s: s.strip(), instr)
+        self.cond = map(lambda s: s.strip(), cond)
+        pass
+
+    def check_condition(self, registers):
+        reg, op, val = self.cond
+        reg_val = registers[reg]
+        int_val = int(val)
+        return self.ops[op](reg_val, int_val)
+
+    def run_instruction(self, registers):
+        reg, op, val = self.instr
+        reg_val = registers[reg]
+        int_val = int(val)
+        registers[reg] = self.ops[op](reg_val, int_val)
+
+    def run(self, registers):
+        if self.check_condition(registers):
+            self.run_instruction(registers)
+
+
+def parse_line(line):
+    instr, cond = [s for s in line.strip().split("if") if s != ""]
+    instrs = tuple([s for s in re.split("\\s", instr.strip()) if s != ""])
+    conds = tuple([s for s in re.split("\\s", cond.strip()) if s != ""])
+    return Instruction(instrs, conds)
+
+
+def highest_register_value(registers):
+    r = sorted(registers.iteritems(), key=itemgetter(1), reverse=True)
+    return r[0][1]
+
+
+def solve_jump_instructions(lines):
+    instructions = [parse_line(line) for line in lines]
+
+    registers = defaultdict(lambda: 0)
+    hi = 0
+    for instr in instructions:
+        instr.run(registers)
+        hi = max(hi, highest_register_value(registers))
+
+    return hi
+
+
+def main():
+    print(solve_jump_instructions(sys.stdin))
+
+
+if __name__ == "__main__":
+    main()

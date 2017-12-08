1954841a46393e1ae96d218d72066cadeeaed16f
Borin Ouch <borin.ouch@gmail.com>
2017-12-07 22:51:11 -0800

    Add day 5 solutions

---
+++ day5b_stranger_trampolines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+
+
+def solve_jumps(instr):
+    i = 0
+    j = 0
+    while i >= 0 and i < len(instr):
+        c = instr[i]
+        instr[i] += -1 if c >= 3 else 1
+        i += c
+        j += 1
+    return j
+
+
+def main():
+    print(solve_jumps([int(n) for n in sys.stdin if n.strip() != ""]))
+
+
+if __name__ == "__main__":
+    main()
